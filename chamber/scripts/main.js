a84e602f2bd933eedb3136989050ff9c5b5b0638
EzequielRindello <rindelloezequiel@gmail.com>
2023-10-11 10:36:33 -0300

    dark mode added

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,4 @@
+// footer currentDate and lastModifiedDate
 const options = {
     month: "short",
     day: "numeric",
@@ -23,3 +24,29 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+// obten el botón y añade un escuchador de eventos para cambiar los colores
+const colorButton = document.getElementById("dark-mode");
+
+colorButton.addEventListener("click", function () {
+    document.body.classList.toggle("color-change");
+    const isColorChanged =
+        document.body.classList.contains("color-change");
+    localStorage.setItem("colorChanged", isColorChanged);
+});
+
+// aplicar las preferencias guardadas al cargar la página
+document.addEventListener("DOMContentLoaded", function () {
+    const isColorChanged =
+        localStorage.getItem("colorChanged") === "true";
+    if (isColorChanged) {
+        document.body.classList.add("color-change");
+    }
+});
+
+// guardar las preferencias antes de que la pagina se cierre
+window.addEventListener("beforeunload", function () {
+    const isColorChanged =
+        document.body.classList.contains("color-change");
+    localStorage.setItem("colorChanged", isColorChanged);
+});
