73288807935ca9a26db52676bd964b0c9f6cc551
EzequielRindello <rindelloezequiel@gmail.com>
2023-10-27 22:50:42 -0300

    adelantando

--- lesson09/scripts/api.js
+++ lesson09/scripts/api.js
@@ -16,3 +16,16 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+const options = {
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+};
+
+const currentDate = document.querySelector('#date-placeholder');
+const formattedDate = new Date().toLocaleDateString("en-US", options);
+currentDate.textContent = formattedDate;
+
+const lastModifiedDate = new Date(document.lastModified).toLocaleDateString("en-US", options);
+document.getElementById("lastModified-placeholder").textContent = lastModifiedDate;
