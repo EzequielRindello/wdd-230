5d78dfdb81a76d958b57974b72ff0f77bb3e2076
EzequielRindello <rindelloezequiel@gmail.com>
2023-11-07 18:01:29 -0300

    json week 09

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,4 +1,4 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const url = 'prophets.json';
 const cards = document.querySelector('#cards');
 
 async function getProphetData() {
