a4441da1d8d8e574dbd82a5817903697b800da3f
EzequielRindello <rindelloezequiel@gmail.com>
2023-11-30 09:27:20 -0300

    menu de hamburguesa

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -6,3 +6,17 @@ const options = {
 const currentDate = document.querySelector('#date-placeholder');
 const formattedDate = new Date().toLocaleDateString("en-US", options);
 currentDate.textContent = formattedDate;
+
+
+// movile menu de hamburguesa
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const navLinks = document.querySelector(".navbar");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+        // cada vez que pasa un addEventListener() cambia el i
+        menuIcon.querySelector("i").classList.toggle("bi-list");
+        menuIcon.querySelector("i").classList.toggle("bi-x");
+    });
+});
