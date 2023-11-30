8311c781801f27bccc489e8c4dc262cb901406eb
EzequielRindello <rindelloezequiel@gmail.com>
2023-11-30 11:03:18 -0300

    weather div

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -20,3 +20,33 @@ document.addEventListener("DOMContentLoaded", function () {
         menuIcon.querySelector("i").classList.toggle("bi-x");
     });
 });
+
+// weather forecast and info
+const apiKey = "942cb3e734cbba69ea8a179cac8e553d";
+const encodedCity = encodeURIComponent("Cozumel, MX");
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${encodedCity}&appid=${apiKey}`;
+
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const weatherDescription = document.getElementById("weather-description");
+const oneDayForecast = document.querySelector(".one-day-forecast");
+const twoDayForecast = document.querySelector(".two-day-forecast");
+
+async function checkWeather() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        // Aquí va el resto del código para manejar los datos del clima.
+
+    } catch (error) {
+        console.error("Error al obtener datos del clima:", error);
+    }
+}
+
+checkWeather();
