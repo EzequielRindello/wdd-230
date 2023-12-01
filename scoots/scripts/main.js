39c28c7c6d2690296ee67ee0a0b9b93588f4ba9c
EzequielRindello <rindelloezequiel@gmail.com>
2023-12-01 10:01:56 -0300

    json table

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -55,3 +55,52 @@ async function checkWeather() {
 }
 
 checkWeather();
+
+// JSON table
+const url = 'data/data.json';
+const tableContainer = document.querySelector('#json-table');
+const cards = document.querySelector('#cards');
+
+async function generateRentalTable() {
+    const displayRentalOptions = (options) => {
+        // Create table, tbody, and header rows
+        let table = document.createElement('table');
+        let tbody = document.createElement('tbody');
+        let headerRows = [
+            '<tr><td colspan="6"><strong>Max Rental Pricing</strong></td></tr>',
+            '<tr><td colspan="2"></td><td colspan="2"><strong>Reservation</strong></td><td colspan="2"><strong>Walk-In</strong></td></tr>',
+            '<tr><td><strong>Rental Type</strong></td><td><strong>Max. Persons</strong></td><td><strong>Half Day<br>(3 hrs)</strong></td><td><strong>Full Day</strong></td><td><strong>Half Day<br>(3 hrs)</strong></td><td><strong>Full Day</strong></td></tr>'
+        ];
+
+        headerRows.forEach((row) => {
+            tbody.innerHTML += row;
+        });
+
+        // Add the data rows
+        options.forEach((option) => {
+            let row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${option.nombre}</td>
+                <td>${option.maximodepersonas}</td>
+                <td>${option.mediodia.precio}</td>
+                <td>${option.fullday.precio}</td>
+                <td>${option.mediodia.precio}</td>
+                <td>${option.fullday.precio}</td>
+            `;
+            tbody.appendChild(row);
+        });
+
+        // Append the tbody to the table
+        table.appendChild(tbody);
+
+        // Append the table to the container
+        tableContainer.appendChild(table);
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    displayRentalOptions(data.opcionesderenta);
+}
+
+generateRentalTable();
+
