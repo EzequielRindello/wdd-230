2e2f406654ab20329ce575f08233a44fac21182c
EzequielRindello <rindelloezequiel@gmail.com>
2023-12-07 20:53:31 -0300

    wather api

--- scoots/scripts/main.js
+++ scoots/scripts/main.js
@@ -28,37 +28,82 @@ function redirectTo(url) {
 
 // weather forecast and info
 const apiKey = "942cb3e734cbba69ea8a179cac8e553d";
-const encodedCity = encodeURIComponent("Cozumel, MX");
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${encodedCity}&appid=${apiKey}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=Mexico&appid=${apiKey}`;
 
 const city = document.querySelector(".city");
-const temp = document.querySelector(".temp");
+const currentTemp = document.querySelector(".temp");
 const weatherDescription = document.getElementById("weather-description");
-const oneDayForecast = document.querySelector(".one-day-forecast");
-const twoDayForecast = document.querySelector(".two-day-forecast");
+const oneDayForecast = document.querySelector(".forecast");
 
-async function checkWeather() {
+async function apiFetch() {
     try {
         const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Error en la solicitud: ${response.status}`);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+
+            // Extract relevant information
+            const cityName = data.city.name;
+            const currentTemperature = data.list[0].main.temp;
+            const weatherDesc = data.list[0].weather[0].description;
+
+            // Display information
+            city.textContent = `Today: ${cityName}`;
+            currentTemp.textContent = `${currentTemperature} °C`;
+            weatherDescription.textContent = `- ${weatherDesc}`;
+
+            // Display forecast for the next two days
+            displayForecast(data.list.slice(0, 16), oneDayForecast);
+            
+        } else {
+            throw Error(await response.text());
         }
-
-        const data = await response.json();
-        console.log(data);
-
-        // Aquí va el resto del código para manejar los datos del clima.
-
     } catch (error) {
-        console.error("Error al obtener datos del clima:", error);
+        console.error(error);
     }
 }
 
-checkWeather();
+function displayForecast(forecastData, element) {
+    // Clear previous content
+    element.innerHTML = "";
+
+    // Loop through the forecast data, grouping by day
+    const groupedByDay = {};
+    forecastData.forEach(item => {
+        const date = new Date(item.dt * 1000); // Convert timestamp to Date object
+        const day = date.toLocaleDateString('en-US', { weekday: 'long' }); // Get day name
+
+        // Group by day
+        if (!groupedByDay[day]) {
+            groupedByDay[day] = [];
+        }
+
+        groupedByDay[day].push(item);
+    });
+
+    // Display the average temperature and weather description for each day
+    Object.keys(groupedByDay).forEach(day => {
+        const dayForecast = groupedByDay[day];
+
+        // Calculate average temperature
+        const averageTemp = dayForecast.reduce((sum, item) => sum + item.main.temp, 0) / dayForecast.length;
+
+        // Get the weather description from the first item of the day
+        const weatherDesc = dayForecast[0].weather[0].description;
+
+        // Display the information
+        const dayElement = document.createElement("div");
+        dayElement.textContent = `${day}: Average Temperature: ${averageTemp.toFixed(2)} °C, Weather: ${weatherDesc}`;
+        element.appendChild(dayElement);
+    });
+}
+
+
+apiFetch();
 
 // JSON table
 const url = 'data/data.json';
-const tableContainer = document.querySelector('#json-table');
+const tableContainer = document.getElementById('json-table');
 
 async function generateRentalTable() {
     const displayRentalOptions = (options) => {
@@ -102,6 +147,7 @@ async function generateRentalTable() {
 }
 
 generateRentalTable();
+
 
 const cards = document.querySelector('#cards');
 // JSON card
